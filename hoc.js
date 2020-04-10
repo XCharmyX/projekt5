ac7740fa099b6c458ae15118542d39dde6b121ab
Bakke94 <58519183+Bakke94@users.noreply.github.com>
2020-04-10 13:45:05 +0200

    carousel

--- hoc.js
+++ hoc.js
@@ -10,6 +10,14 @@ function validateForm() {
   }
 }
 
+let liEls = document.querySelectorAll('ul li');
+let index = 0;
+window.show = function(increase) {
+  index = index + increase;
+  index = Math.min(Math.max(index,0), liEls.length-1);
+  liEls[index].scrollIntoView({behavior: 'smooth'});
+}
+
 
 // js til multiplechoice formular
 
